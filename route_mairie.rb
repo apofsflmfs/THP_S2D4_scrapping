b00157650f82535943c2a409e7394746aa830909
Clem Baldy <clemoun@yahoo.fr>
2018-10-11 16:12:17 +0200

    modif sur rout_mairie et ajout de trader

--- route_mairie.rb
+++ route_mairie.rb
@@ -3,8 +3,9 @@ require 'open-uri'
 
 def get_the_email_of_a_townhall_from_its_webpage(townhall_url)
   page = Nokogiri::HTML(open(townhall_url))
-  return page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-
+  email = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+  puts email
+  return email
 end
 
 def get_all_the_urls_of_val_doise_townhalls(department_url)
@@ -12,7 +13,6 @@ def get_all_the_urls_of_val_doise_townhalls(department_url)
   a_elements = page.xpath('//a')
 
   result = {}
-
   a_elements.each do |a_element|
     link = a_element['href']
     if link.include?('./95/')
@@ -24,20 +24,15 @@ def get_all_the_urls_of_val_doise_townhalls(department_url)
   return result
 end
 
-def get_all_the_emails_of_val_doise_townhalls(url)
+def get_all_the_emails_of_val_doise_townhalls
+  val_doise_emails = []
 
-  val_doise_emails = {}
-
-  get_all_the_urls_of_val_doise_townhalls(url).each do |townhall_name, townhall_url|
-    val_doise_emails[townhall_name] = get_the_email_of_a_townhall_from_its_webpage(townhall_url)
+  get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html").each do |townhall_name, townhall_url|
+    val_doise_emails << {"name" => townhall_name, "email" => get_the_email_of_a_townhall_from_its_webpage(townhall_url)}
   end
 
   return val_doise_emails
 end
 
-def perform
-  # puts get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html")
-  puts get_all_the_emails_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html").select {|k,v| v==""}
-end
-
-perform
+  puts get_all_the_emails_of_val_doise_townhalls
+  # puts get_all_the_emails_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html").select {|k,v| v==""}
---
+++ trader.rb
@@ -0,0 +1,57 @@
+require 'nokogiri'
+require 'open-uri'
+
+filename = "file.txt"
+@storage_file = open(filename, 'w')
+
+
+#technique où on passe par la page de la monnaie pour obtenir le cours de la monnaie => C'est plus rapide
+def get_crypto_value_short(coinmarket_url)
+  page = Nokogiri::HTML(open(coinmarket_url))
+  puts "page chargée =>> ok"
+  crypto_as = page.xpath('//a[contains(@class, "currency-name-container")]')
+  prices = page.xpath('//a[@class = "price"]')
+  result ={}
+  crypto_as.each_with_index do |crypto_a, index|
+    crypto_name = crypto_a.text
+    crypto_price = prices[index].text
+    result[crypto_name] = crypto_price
+    end
+  return result
+end
+
+def perform
+  i = 1
+  while(true)
+    puts "boucle n°#{i} lancée"
+    @storage_file.write("\n") 
+    @storage_file.write("*********************enregistrement de #{Time.now.strftime("%d/%m/%Y %H:%M")}*********************")
+    @storage_file.write("\n") 
+    @storage_file.write(get_crypto_value_short('https://coinmarketcap.com/all/views/all/'))
+
+    sleep(1800)
+    i +=1
+  end
+end
+
+perform
+
+
+
+#technique où on passe par la page de la monnaie pour obtenir le cours de la monnaie => C'est looooooooooooong
+
+def get_value_from_url(crypto_url)
+  page = Nokogiri::HTML(open(crypto_url))
+  return page.xpath('//*[@id="quote_price"]/span[1]').text
+end
+
+def get_crypto_value_loooong(coinmarket_url)
+  page = Nokogiri::HTML(open(coinmarket_url))
+  crypto_as = page.xpath('//a[contains(@class, "currency-name-container")]')
+  result ={}
+  crypto_as.each do |crypto_a|
+    link_crypto = "https://coinmarketcap.com"+crypto_a['href']
+    result[crypto_a.text] = get_value_from_url(link_crypto)
+    end
+  return result
+end
