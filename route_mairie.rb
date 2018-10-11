fc94f00093778c98d24fb4d2287f89dfcdfb9b51
Clem Baldy <clemoun@yahoo.fr>
2018-10-11 20:54:45 +0200

    fichiers texts sauvant les infos

--- route_mairie.rb
+++ route_mairie.rb
@@ -1,6 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
+@storage_file = open("townhall_emails.txt", 'w')
+
+
 def get_the_email_of_a_townhall_from_its_webpage(townhall_url)
   page = Nokogiri::HTML(open(townhall_url))
   email = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
@@ -30,7 +33,7 @@ def get_all_the_emails_of_val_doise_townhalls
   get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html").each do |townhall_name, townhall_url|
     val_doise_emails << {"name" => townhall_name, "email" => get_the_email_of_a_townhall_from_its_webpage(townhall_url)}
   end
-
+  @storage_file.write(val_doise_emails)
   return val_doise_emails
 end
 
